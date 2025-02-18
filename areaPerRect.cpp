8a59717a5506a03d6c4fc8bdd5148ccde4c60011
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-02-18 22:03:42 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2025. Joseph Wondimagnehu. All rights reserved.
+//
+// Created by: Joseph Wondimagnehu
+// Date: Feb. 18, 2025
+// This program calculates the area and perimeter of any rectangle.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+/* A function stating the area and perimeter of a rectangle. 
+This returns nothing, so it is a void type. */
+void calcAreaPerimeter(float l, float w) {
+    cout << "If a rectangle has the dimensions: \n";
+    cout << l << " cm x " << w << "cm \n\n";
+    cout << "The area is: " << (l * w) << "cm^2 \n";
+    cout << "The perimeter is: " << (2 * (l + w)) << "cm \n";
+}
+
+int main() {
+    // Call this function with an example 13cm x 12cm rectangle.
+    calcAreaPerimeter(13, 12);
+}
